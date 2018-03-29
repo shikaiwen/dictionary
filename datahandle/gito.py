97ef46f2a8f7a7399f6b017d6c01a4d962b98cc3
kevin <shikw@sinfo.sinfo-inc.co.jp>
2018-03-29 18:02:30 +0900

    22

---
+++ datahandle/gito.py
@@ -0,0 +1,39 @@
+import sys
+import subprocess
+
+# print(sys.argv)
+cmdlist = [
+    "git add *",
+    "git commit -m \"\" ",
+    "git push"
+    ]
+for i in range(len(cmdlist)):
+    exe = cmdlist[i]
+    child = subprocess.Popen(exe, stdout = subprocess.PIPE, stderr = subprocess.PIPE, shell=False)
+#    None 好像也是成功
+    if(child.returncode != 0 and child.returncode != None):  
+        print(child.communicate()[0])
+        exit()
+#         
+
+# print("$$$$$$$$$$$$$$$ " + str(child.communicate()[0]))
+# print("$$$$$$$$$$$$$$$ " + str(child.returncode))
+
+
+ress = subprocess.check_output("git add *",stderr=subprocess.STDOUT,shell=True)
+print(ress)
+exit()
+
+if(len(sys.argv) == 1):
+    cmdlist = [
+        "git add *",
+        "git commit -m 'simple commit'",
+        "git push"
+        ]
+    
+#     cmd = "git add * \n git commit -m 'simple commit' "
+#     cmd = "git status \n"
+#     cmd = "ls"
+    
+    subprocess.call("\n&&".join(cmdlist), shell=False)
+    
