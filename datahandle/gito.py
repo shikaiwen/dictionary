425bd717b71669d180b8503b0cbc6fcdd8cf7c99
kevin <shikw@sinfo.sinfo-inc.co.jp>
2018-03-29 18:31:54 +0900

    df

--- datahandle/gito.py
+++ datahandle/gito.py
@@ -1,7 +1,7 @@
 import sys
 import subprocess
 from sys import stdin
-
+# https://stackoverflow.com/questions/14457303/python-subprocess-and-user-interaction
 # print(sys.argv)
 cmdlist = [
     "git add *",
@@ -15,15 +15,15 @@ for i in range(len(cmdlist)):
 #    None 好像也是成功
     if(child.returncode != 0 and child.returncode != None):  
         retuple = child.communicate() 
-        print(retuple[0])
-        exit()
+#         print(retuple[0])
+#         exit()
 #         
 
 # print("$$$$$$$$$$$$$$$ " + str(child.communicate()[0]))
 # print("$$$$$$$$$$$$$$$ " + str(child.returncode))
 
-ress = subprocess.check_output("git add *",stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=subprocess.PIPE,shell=True)
-print(ress)
+# ress = subprocess.check_output("git add *",stdout=subprocess.PIPE,stderr=subprocess.STDOUT,stdin=subprocess.PIPE,shell=True)
+# print(ress)
 exit()
 
 if(len(sys.argv) == 1):
